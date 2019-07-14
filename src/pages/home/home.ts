8b5d0aad8e3767f8471b03eb432d014440730788
Lucasolifreitas <lucasolifreitas@outlook.com>
2019-07-14 16:38:36 -0300

    pequenas atualizações

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,7 +3,7 @@ import { Storage } from "@ionic/storage";
 import { Component, ViewChild } from '@angular/core';
 import { Content } from 'ionic-angular';
 
-import { Starwars} from '../../providers/starwars/starwars';
+import { Starwars } from '../../providers/starwars/starwars';
 
 @Component({
   selector: 'page-home',
@@ -22,18 +22,18 @@ export class HomePage {
   data_films: any;
   tamanho: any;
   page: any;
-  
+
   constructor(public starwars: Starwars,
     public storage: Storage) {
-    
+
   }
 
   getPeople() {
     this.starwars.getPeople(this.id_people)
-      .then((result: any) => {    
+      .then((result: any) => {
         this.responseData = result;
         const dados = this.responseData;
-  
+
         if (dados) {
           this.storage.set('dados', JSON.stringify(dados));
           this.people = dados;
@@ -41,19 +41,19 @@ export class HomePage {
         }
 
       })
-      
+
   }
 
   getPlanet() {
-    if(this.page == "" || this.page == undefined){
+    if (this.page == "" || this.page == undefined) {
       this.page = 1
     }
-    console.log("get_planet" + this.page) 
+    console.log("get_planet" + this.page)
     this.starwars.getPlanet(this.page)
-      .then((result: any) => {    
+      .then((result: any) => {
         this.responseData = result;
         const dados = this.responseData;
-  
+
         if (dados) {
           this.storage.set('dados', (dados));
           this.data_planet = dados["results"];
@@ -61,59 +61,59 @@ export class HomePage {
           console.log(this.data_planet);
           console.log(this.data_films);
           this.tamanho = this.data_planet.length;
-         
-        }        
 
-      })      
+        }
+
+      })
   }
 
 
-/*
-teste(){
-  let filmes_por_planeta = {}
-  for (let i = 0; i <10; i++) {
-    let filmes = []
-    for(let j = 0; j < 2; j++){
-      filmes[j] = j
-      console.log(j); 
+  /*
+  teste(){
+    let filmes_por_planeta = {}
+    for (let i = 0; i <10; i++) {
+      let filmes = []
+      for(let j = 0; j < 2; j++){
+        filmes[j] = j
+        console.log(j); 
+      }
+      filmes_por_planeta[i] = {filmes}
+      console.log(i);     
+  }
+  console.log(filmes_por_planeta)
+  }
+  */
+  proximo() {
+    this.page = this.page + 1
+    if (this.page > 7) {
+      alert("You've seen all the planets!")
+      this.page = 7
     }
-    filmes_por_planeta[i] = {filmes}
-    console.log(i);     
-}
-console.log(filmes_por_planeta)
-}
-*/
-proximo(){
-this.page = this.page + 1
-if(this.page > 7){
-  alert("You've seen all the planets!")
-  this.page = 7
-}
-console.log(this.page)
-this.getPlanet()
-this.scrollToTop();
-}
-
-anterior(){
-this.page = this.page - 1
-console.log(this.page)
-this.getPlanet()
-this.scrollToTop();
-
-}
-
-home(){
-  this.page = 1
-  console.log(this.page)
-  this.getPlanet()
-  this.scrollToTop();
-  
+    console.log(this.page)
+    this.getPlanet()
+    this.scrollToTop();
   }
 
-scrollToTop() {
-  this.content.scrollToTop();
-}
-  
+  anterior() {
+    this.page = this.page - 1
+    console.log(this.page)
+    this.getPlanet()
+    this.scrollToTop();
+
+  }
+
+  home() {
+    this.page = 1
+    console.log(this.page)
+    this.getPlanet()
+    this.scrollToTop();
+
+  }
+
+  scrollToTop() {
+    this.content.scrollToTop();
+  }
+
 
 
   id(id: any) {
@@ -123,9 +123,9 @@ scrollToTop() {
 
 
 
-  ionViewWillEnter(){
-  this.getPlanet()
- 
+  ionViewWillEnter() {
+    this.getPlanet()
+
   }
 
 }
